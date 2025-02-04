0f6aa79a3ed21a831ff9b332cffacfc09dd7ac5e
Antonio Henrique <antoniohenriqueferreira@outlook.com.br>
2025-02-03 21:00:47 -0300

    Meu segundo commit - Criando função adicionarAmigo() e atualizarLista()

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
 // Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    let novoAmigo = document.getElementById("amigo").value;
+    if(novoAmigo !== ""){
+        amigos.push(novoAmigo);
+        atualizarLista();
+        document.getElementById("amigo").value = "";
+    } else {
+        alert("Por favor, digite um nome válido! O campo não pode estar vazio!");
+    };
+};
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement("li");
+        item.innerText = amigos[i]; 
+        lista.appendChild(item); 
+    };
+};
+
+
